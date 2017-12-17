a99e69d794f0c604f7379ac17d6859532e414810
Melissa O'Connor <mmoconnor23@gmail.com>
2017-12-17 17:13:47 -0500

    feat(cors): add npm package

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
 import * as express from 'express';
+import * as cors from 'cors';
 
 const app = express();
+ 
+app.use(cors());
 
-function allowCORS(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-}
-
-app.configure(() => {
-  app.use(allowCrossDomain);
-});
+//enable pre-flight across-the-board
+app.options('*', cors());
 
 app.post('/addrecipe', (req, res) => {
   console.log('hit a route', req, res);
