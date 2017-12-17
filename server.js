265356e2d38fbe7f5b8c56ca389ac6b4bbbdd8f2
Melissa O'Connor <mmoconnor23@gmail.com>
2017-12-17 16:57:31 -0500

    fix(header): specify localhost

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.all('/addrecipe', (req, res) => {
   console.log('hit a route', req, res);
-  res.addHeader('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.addHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.addHeader('Access-Control-Allow-Headers', 'X-Requested-With');
   res.send('melissa');
 });
 
